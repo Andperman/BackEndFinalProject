75b2e4c6b14c1b3fc4c645360dadfe1d9376e991
Andrea Perez Manzaneque <andperman@Ordenador-portatil-de-Andrea.local>
2024-11-11 14:02:50 +0100

    authRoutes

---
+++ routes/authRoutes.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const { register, login, logout } = require('../controllers/authController');
+const { getProfile, getFavorites } = require('../controllers/userController');
+const { getUsers, createAd, getDashboard } = require('../controllers/adminController');
+const authMiddleware = require('../middlewares/authMiddleware');
+const authorizeRole = require('../middlewares/roleMiddleware');
+
+// Rutas de autenticación
+router.get('/register', (req, res) => res.render('register'));  // Registro
+router.post('/register', register);                              // Registrar usuario
+router.get('/login', (req, res) => res.render('login'));         // login
+router.post('/login', login);                                    // Login usuario
+router.get('/logout', logout);                                   // Logout usuario
+
+// Rutas de usuario (requiere autenticación)
+router.get('/profile', authMiddleware, getProfile);              // Perfil de usuario
+router.get('/favorites', authMiddleware, getFavorites);          // Ofertas favoritas
+
+// Rutas de administrador (requiere autenticación y rol de 'admin')
+router.get('/admin/dashboard', authMiddleware, authorizeRole('admin'), getDashboard); // Dashboard admin
+router.get('/admin/users', authMiddleware, authorizeRole('admin'), getUsers);         // Listado de usuarios
+router.post('/admin/ads', authMiddleware, authorizeRole('admin'), createAd);          // Crear anuncio
+
+module.exports = router;
