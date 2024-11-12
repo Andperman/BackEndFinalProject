5721b18979a94a1c17facabb2aa87e8785301c97
Andrea Perez Manzaneque <andperman@Ordenador-portatil-de-Andrea.local>
2024-11-12 13:22:04 +0100

    ruta/scrapeSave

--- routes/jobOffers.routes.js
+++ routes/jobOffers.routes.js
@@ -1,9 +1,11 @@
 const jobOffersController = require('../controllers/jobOffers.controller');
 const router = require('express').Router();
+const express = require('express');
 
 router.get("/:id?", jobOffersController.getAllJobOffers);
 router.post("/", jobOffersController.createJobOffer);
 router.put("/:id", jobOffersController.updateJobOffer);
 router.delete("/:id", jobOffersController.deleteJobOffer);
+router.post('/scrapeSave', jobOffersController.scrapAndSaveJobOffers);
 
 module.exports = router;
