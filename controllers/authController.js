bde739dbb3826a0339d405a8690b874985596478
Andrea Perez Manzaneque <andperman@Ordenador-portatil-de-Andrea.local>
2024-11-14 20:37:31 +0100

    comentarios

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,22 +1,8 @@
 //funciones donde gestionamos la autentificación y autorización de los usuarios
 // Importamos la función que busca usuarios por email
-const { createUser, getUsersByEmail } = require('../models/user.model');// interactuar con la base de datos
+const { createUser, getUsersByEmail } = require('../models/user.model');
 const jwt = require('jsonwebtoken');  // JWT generar tokens
-const bcrypt = require('bcryptjs');  // Bcrypt para cifrar contraseñas
-
-// // Registro de usuario
-// //Creamos un nuevo usuario en la base de datos, encriptando su contraseña con bcryptjs antes de guardarla.
-// async function register(req, res) {
-//     const { username, password, email,img , role = 'user' } = req.body; // role se establece por defecto como 'user'
-//     // console.log('Password recibido:', password);
-//     try {
-//         // Crear nuevo usuario en la base de datos
-//         const newUser = await createUser(username, password, email,img, role);
-//         res.redirect('/');  // Redireccionar al login si todo sale bien 
-//     } catch (error) {
-//         res.status(500).send('Error en el registro'); 
-//     }
-// }
+const bcrypt = require('bcryptjs');  
 
 // Inicio de sesión
 async function login(req, res) {
@@ -54,7 +40,7 @@ async function login(req, res) {
 function logout(req, res) {
     // Elimina la cookie del token
     res.clearCookie('token');
-    res.redirect('/login');  // Redireccionar al login después del logout
+    res.redirect('/login'); 
 }
 
 
@@ -63,7 +49,7 @@ async function googleLogin(req, res) {
         const { id, displayName, emails } = req.user;  // Los datos del usuario de Google
 
         // Si displayName no hay, utiliza el correo electrónico
-        const username = displayName || emails[0].value.split('@')[0]; // Usa displayName, si no existe usa el correo sin el dominio
+        const username = displayName || emails[0].value.split('@')[0]; 
 
         // Verificar si el usuario ya existe en la base de datos por su email
         let users = await getUsersByEmail(emails[0].value);  // Buscar el usuario por su correo electrónico
@@ -91,7 +77,6 @@ async function googleLogin(req, res) {
             sameSite: 'strict'
         });
 
-        // Redirigir al dashboard
         res.redirect('/');
     } catch (error) {
         console.error(error);
--- utils/scraper.js
+++ utils/scraper.js
@@ -9,19 +9,11 @@ const extractProductData = async (url, browser) => {
         await page.goto(url)
 
         // // Extraemos los datos de cada anuncio
-        // productData['title'] = await page.$eval('#projects > div:nth-child(1) > div.project-header > h2 > span > a > span', title => title.innerText);
-        // // // productData['description'] = await page.$eval("")
-       // Espera a que el selector esté presente
        productData['title'] = await page.$eval("#productName > h1", name => name.innerHTML.trim().replace(/\n+/g, ' ').replace(/\s+/g, ' '));
        productData['description'] = await page.$eval("#app > div > div.container.main > section > section > div > section > article:nth-child(1) > div.expander.js-expander-passed", description => description.innerText.slice(0,400).trim().replace(/\n+/g, ' ').replace(/\s+/g, ' ') + '...');
        productData['date'] = await page.$eval("#productName > p", data => data.innerHTML.trim().replace(/\n+/g, ' ').replace(/\s+/g, ' '));
        productData['website'] = await page.url();
-       
-
        await page.close();
-      
-
-
         return productData; 
 
     } catch (err) {
@@ -33,31 +25,16 @@ const scrap = async (url) => {
     try {
       // Creamos un array vacío scrapedData donde almacenaremos la información obtenida del scraping (se alamcenan los objetos)
       const scrapedData = []
-      // inicializamos una instancia del navegador (browser) con puppeteer.launch() y añadimos en el objeto de configuración la opción headless
       console.log("Opening the browser......");
       const browser = await puppeteer.launch({headless:true})
-
-      // Abrimos una nueva pestaña en el navegador creando una instancia con el método newPage() a la que llamaremos page
       const page = await browser.newPage();
-      // Indicamos la url que debe cargarse en la pestaña con page.goto(url)
       await page.goto(url);
       console.log(`Navigating to ${url}...`);
 
-      // Extraemos todos los links a los que luego navegaremos para obtener el detalle de cada producto
-
-      // Utilizamos el método $$eval(selector, callback) para capturar una colección de nodos y aplicar la lógica que necesitemos
-      // En este caso , en el CB filtramos el array de items, guardando en un nuevo array
-
-      /********** A RELLENAR page.$eval(selector, function)  *********/
       const tmpurls = await page.$$eval("h2.project-title a", links => links.map(a => a.href));
-
-      
-    //   const tmpurls = await page.$$eval("#projects > div:nth-child(1) > div.project-header > h2 > span > a" , link => link.map (a => a.href))
-      //Quitamos los duplicados
       const urls = await tmpurls.filter((link,index) =>{ return tmpurls.indexOf(link) === index})
 
       console.log("url capuradas",urls)
-      // Me quedo con los 20 primeros productos, porque sino es muy largo
       const urls2 = urls.slice(0, 20);
 
       for(productLink in urls2){
@@ -67,7 +44,7 @@ const scrap = async (url) => {
     
     console.log(scrapedData, "Lo que devuelve mi función scraper", scrapedData.length) 
    
-    // cerramos el browser con el método browser.close
+    
     await browser.close()
     // Devolvemos el array con los productos
     return scrapedData;
@@ -131,9 +108,9 @@ const scrapeFreelancerJobs = async (url) => {
 
       console.log(scrapedData, "Datos obtenidos de los trabajos freelance", scrapedData.length);
 
-      await browser.close(); // Cerramos el navegador
+      await browser.close();
 
-      return scrapedData; // Devolvemos los datos obtenidos
+      return scrapedData; 
 
     } catch (err) {
         console.log("Error:", err);
