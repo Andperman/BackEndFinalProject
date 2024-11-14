d34004d56bf423a5b65a066417a0a086abf16d28
Deydre <deydrealonso@gmail.com>
2024-11-14 21:19:38 +0100

    add/home now shows all offers and then you can filter

--- controllers/jobOffers.controller.js
+++ controllers/jobOffers.controller.js
@@ -97,7 +97,6 @@ const scrapAndSaveJobOffers = async (req, res) => {
             }
         }
 
-
         // Responde con los anuncios guardados en MongoDB
         res.status(201).json({
             message: "Job offers scrapped and saved successfully",
--- public/js/script.js
+++ public/js/script.js
@@ -66,10 +66,13 @@ if (document.querySelector("#popupAddUser")) {
     };
 }
 
+// Variable global que declaramos porque la comprobará en distintas funciones
+let favoritesUser;
 
-// #buttonsDashboardUsers
+// ------------------- OTRAS INTERACCIONES CON EL USUARIO -------------------
 
 // FORMULARIO DE BÚSQUEDA -> PINTAR OFERTAS
+// Listener en el formulario para activar paintOffers(búsqueda)
 if (document.querySelector('#formResults')) {
     let form = document.querySelector('#formResults');
 
@@ -80,54 +83,80 @@ if (document.querySelector('#formResults')) {
     });
 }
 
-// Variable global que declaramos porque la comprobará en distintas funciones
-let favoritesUser;
-
-// OBTENER FAVORITOS DE UN USUARIO CONCRETO
+// (FETCH) OBTENER FAVORITOS DE UN USUARIO CONCRETO
 const getFavorites = async () => {
-    // Comprobar email en la cookie
+    // Falta:
+    // -- Comprobar email en la cookie
     // -- Fetch a SQL para obtener ID del User
 
-    // OBTENER FAVORITOS DEL USUARIO
+    // Obtener favoritos del usuario
     // Fetch a SQL para obtener los favoritos del User
     let responseSQL = await fetch('/api/favorites/1')
     favoritesUser = await responseSQL.json();
 
     return favoritesUser;
 }
-// OBTENER TODOS LOS USUARIOS
+
+// (FETCH) OBTENER TODOS LOS USUARIOS
 const getAllUsers = async () => {
     let responseSQL = await fetch('/api/user/');
     allUsers = await responseSQL.json();
     return allUsers;
 }
 
-
-// PINTAR SÓLO FAVORIOS DE UN USUARIO CONCRETO
-const paintFavorites = async () => {
-    // Obtener lista de favoritos del usuario
-    let favoritesUser = await getFavorites();
-    console.log(favoritesUser)
-
+// (FETCH) OBTENER TODAS LAS OFERTAS DE MONGO
+const getAllOffers = async () => {
     // Obtener lista de ofertas de Mongo
     const responseMongo = await fetch('/api/joboffers');
     // Verificar si la respuesta es exitosa
     if (!responseMongo.ok) {
         throw new Error(`Error HTTP: ${responseMongo.status} - ${responseMongo.statusText}`);
     }
-    const dataMongo = await responseMongo.json();
+    return await responseMongo.json();
+}
 
-    // Iteramos el array de palabras de búsqueda
-    const favorites = dataMongo.filter(offer =>
-        favoritesUser.some(favorite => favorite.mongo_id === offer._id)
-    );
+// MARCAR/DESMARCAR FAVORITO
+const toggleFavorite = async (idMongo, isFavorite) => {
+    let titleMongo = document.getElementById(`${idMongo}`).innerHTML;
+    // Si no es favorito, crearlo
+    if (!isFavorite) {
+        // Añadir favorito
+        await fetch('/api/favorites', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                user_id: 1,
+                mongo_title: titleMongo,
+                mongo_id: idMongo
+            })
+        });
+        getFavorites();
+        // Si es favorito, eliminarlo
+    } else {
+        // Eliminar favorito
+        let favoriteToDelete = favoritesUser.find(favorite => favorite.mongo_id === idMongo);
+        if (favoriteToDelete) {
+            await fetch(`/api/favorites/${favoriteToDelete.favorite_id}`, {
+                method: "DELETE"
+            });
+        }
+        getFavorites();
+    }
 
+    // Refrescar lista de favoritos
+    await getFavorites();
+};
 
-    // Pintar resultados
-    let section = document.querySelector("#divFavorites");
-    section.innerHTML = "";
-    favorites.forEach(result => {
-        //Comprobamos si la oferta está en los favoritos del user
+// (/) PINTAR TODAS LAS OFERTAS SIN FILTRAR
+const paintAllOffers = async () => {
+    await getFavorites()
+    // Obtener todas las ofertas
+    let dataMongo = await getAllOffers();
+    // Pintar resultados iniciales
+    let section = document.querySelector("#divResults");
+    dataMongo.forEach(result => {
         const isFavorite = favoritesUser.some(favorite => favorite.mongo_id === result._id);
         section.innerHTML += `
             <article class="offerArticle">
@@ -156,81 +185,23 @@ const paintFavorites = async () => {
                 
             </article>
         `
-        // Interacción con botones de favorito
-        const heartEmptyButtons = document.getElementsByClassName("heartButtonEmpty");
-        const heartFullButtons = document.getElementsByClassName("heartButtonFull");
-
-        // Agregar listeners para cada botón de favorito
-        for (let i = 0; i < heartEmptyButtons.length; i++) {
-            heartEmptyButtons[i].addEventListener('click', () => {
-                toggleFavorite(heartEmptyButtons[i].getAttribute('id'), false);
-                heartEmptyButtons[i].classList.add("hidden");
-                heartFullButtons[i].classList.remove("hidden");
-                paintFavorites();
-            });
-        }
-
-        for (let i = 0; i < heartFullButtons.length; i++) {
-            heartFullButtons[i].addEventListener('click', () => {
-                toggleFavorite(heartFullButtons[i].getAttribute('id'), true);
-                heartFullButtons[i].classList.add("hidden");
-                heartEmptyButtons[i].classList.remove("hidden");
-                paintFavorites();
-            });
-        }
     })
 }
 
-// MARCAR/DESMARCAR FAVORITO
-const toggleFavorite = async (idMongo, isFavorite) => {
-    let titleMongo = document.getElementById(`${idMongo}`).innerHTML;
-
-    if (!isFavorite) {
-        // Añadir favorito
-        await fetch('/api/favorites', {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                user_id: 1,
-                mongo_title: titleMongo,
-                mongo_id: idMongo
-            })
-        });
-        getFavorites();
-    } else {
-        // Eliminar favorito
-        let favoriteToDelete = favoritesUser.find(favorite => favorite.mongo_id === idMongo);
-        if (favoriteToDelete) {
-            await fetch(`/api/favorites/${favoriteToDelete.favorite_id}`, { method: "DELETE" });
-        }
-        getFavorites();
-    }
-
-    // Refrescar lista de favoritos
-    await getFavorites();
-};
-
-// HOME
-// PINTAR RESULTADOS DE BÚSQUEDA EN EL DOM
+// (/) PINTAR RESULTADOS DE BÚSQUEDA EN EL DOM
 const paintOffers = async (search) => {
     await getFavorites()
 
-    // PINTAR OFERTAS DE MONGODB
-    // Realizar la solicitud a la API
-    const responseMongo = await fetch('/api/joboffers');
-    // Verificar si la respuesta es exitosa
-    if (!responseMongo.ok) {
-        throw new Error(`Error HTTP: ${responseMongo.status} - ${responseMongo.statusText}`);
-    }
-    const dataMongo = await responseMongo.json();
+    // Obtener todas las ofertas
+    let dataMongo = await getAllOffers();
+
     // Transformamos el string que busca el cliente en un array
-    let arraySearch = search.split(' ');
+    let arraySearch = search.split(' '); // ["página", "web"]
     // Iteramos el array de palabras de búsqueda
     arraySearch.forEach(wordSearch => {
         // Iteramos el array que recibimos por Mongo buscando coincidencias entre ellos
         dataFiltered = dataMongo.filter(offer => {
+            // Filtrar coincidencias de 1 palabra por título o descripción
             return offer.title.toLowerCase().split(' ').some(word => word.includes(wordSearch.toLowerCase())) || offer.description.toLowerCase().split(' ').some(word => word.includes(wordSearch.toLowerCase()))
         });
     })
@@ -273,6 +244,7 @@ const paintOffers = async (search) => {
     const heartEmptyButtons = document.getElementsByClassName("heartButtonEmpty");
     const heartFullButtons = document.getElementsByClassName("heartButtonFull");
 
+    // Agregar listeners para cada botón de favorito (vacíos)
     for (let i = 0; i < heartEmptyButtons.length; i++) {
         heartEmptyButtons[i].addEventListener('click', () => {
             toggleFavorite(heartEmptyButtons[i].getAttribute('id'), false);
@@ -281,6 +253,7 @@ const paintOffers = async (search) => {
         });
     }
 
+    // Agregar listeners para cada botón de favorito (llenos)
     for (let i = 0; i < heartFullButtons.length; i++) {
         heartFullButtons[i].addEventListener('click', () => {
             toggleFavorite(heartFullButtons[i].getAttribute('id'), true);
@@ -290,10 +263,82 @@ const paintOffers = async (search) => {
     }
 }
 
-// PINTAR TODOS LOS USUARIOS
+// (/favorites) PINTAR SÓLO FAVORIOS DE UN USUARIO CONCRETO
+const paintFavorites = async () => {
+    // Obtener los ids de favoritos del usuario
+    let favoritesUser = await getFavorites();
+    console.log(favoritesUser)
+
+    // Obtener todas las ofertas
+    let dataMongo = await getAllOffers();
+
+    // Filtrar en todas las ofertas las que coincidan con los ids de favoritos
+    const favorites = dataMongo.filter(offer =>
+        favoritesUser.some(favorite => favorite.mongo_id === offer._id)
+    );
+
+    // Pintar resultados
+    let section = document.querySelector("#divFavorites");
+    section.innerHTML = "";
+    favorites.forEach(result => {
+        //Comprobamos si cada oferta está en los favoritos del user (para ocultar/desocultar el corazón)
+        const isFavorite = favoritesUser.some(favorite => favorite.mongo_id === result._id);
+        section.innerHTML += `
+            <article class="offerArticle">
+                <div>
+                    <div>
+                        <div>
+                            <h2 id=${result._id}>${result.title}</h2>
+                            <p class="date">${result.date}</p>
+                        </div>
+                        <div class="divStars">
+                            <button class="heartButtonFull ${isFavorite ? '' : 'hidden'}" id="${result._id}">
+                                <i class="fa-solid fa-heart"></i>
+                            </button>
+                            <button class="heartButtonEmpty ${isFavorite ? 'hidden' : ''}" id="${result._id}">
+                                <i class="fa-regular fa-heart"></i>
+                            </button>
+                        </div>
+                    </div>
+                    <div>
+                        <p>${result.description}</p>
+                    </div>
+                </div>
+                <div>
+                    <a href="${result.website}" class=viewOfferButton>VIEW OFFER</a>
+                </div>
+                
+            </article>
+        `
+        // Interacción con botones de favorito
+        const heartEmptyButtons = document.getElementsByClassName("heartButtonEmpty");
+        const heartFullButtons = document.getElementsByClassName("heartButtonFull");
+
+        // Agregar listeners para cada botón de favorito (vacíos)
+        for (let i = 0; i < heartEmptyButtons.length; i++) {
+            heartEmptyButtons[i].addEventListener('click', () => {
+                // Mandamos el id a la función toggleFvorite()
+                toggleFavorite(heartEmptyButtons[i].getAttribute('id'), false);
+                heartEmptyButtons[i].classList.add("hidden");
+                heartFullButtons[i].classList.remove("hidden");
+                paintFavorites();
+            });
+        }
+        // Agregar listeners para cada botón de favorito (llenos)
+        for (let i = 0; i < heartFullButtons.length; i++) {
+            heartFullButtons[i].addEventListener('click', () => {
+                toggleFavorite(heartFullButtons[i].getAttribute('id'), true);
+                heartFullButtons[i].classList.add("hidden");
+                heartEmptyButtons[i].classList.remove("hidden");
+                paintFavorites();
+            });
+        }
+    })
+}
+
+// (/users) PINTAR TODOS LOS USUARIOS
 const paintAllUsers = async () => {
     let users = await getAllUsers();
-    console.log(users)
     // Pintar resultados
     let section = document.querySelector("#divAllUsers");
     section.innerHTML = "";
@@ -317,6 +362,7 @@ const paintAllUsers = async () => {
         `
     })
 
+    // Event listener para borrar un user concreto
     let eraseUser = document.getElementsByClassName('eraseUser');
     for (let i = 0; i < eraseUser.length; i++) {
         eraseUser[i].addEventListener('click', async () => {
@@ -332,20 +378,15 @@ const paintAllUsers = async () => {
     }
 }
 
-
-// DASHBOARD
-// PINTAR OFERTAS CREADAS POR EL ADMIN
+// (/dashboard) PINTAR SÓLO OFERTAS CREADAS POR EL ADMIN
 const paintOffersInDashboard = async () => {
-    // PINTAR OFERTAS DE MONGODB
-    // Realizar la solicitud a la API
-    const responseMongo = await fetch('/api/joboffers');
-    // Verificar si la respuesta es exitosa
-    if (!responseMongo.ok) {
-        throw new Error(`Error HTTP: ${responseMongo.status} - ${responseMongo.statusText}`);
-    }
-    const dataMongo = await responseMongo.json();
+
+    /// Obtener todas las ofertas
+    let dataMongo = await getAllOffers();
+
     // Iteramos el array y bsucamos los que estén hechas por el admin
     let dataFiltered = dataMongo.filter(offer => offer.createdBy === "admin");
+
     // Pintar resultados
     let section = document.querySelector("#divDashboard");
     section.innerHTML = "";
@@ -370,6 +411,7 @@ const paintOffersInDashboard = async () => {
         `
 
     })
+    // Borrar oferta en concreto
     let eraseOffer = document.getElementsByClassName('eraseOffer');
     for (let i = 0; i < eraseOffer.length; i++) {
         eraseOffer[i].addEventListener('click', async () => {
@@ -383,6 +425,12 @@ const paintOffersInDashboard = async () => {
             paintOffersInDashboard();
         })
     }
+}
+
+
+// Si estamos en la home, pintar todas las ofertas
+if (document.querySelector("#divResults")) {
+   paintAllOffers();
 }
 
 // Si estamos en el dashboard, pintar ofertas del admin
